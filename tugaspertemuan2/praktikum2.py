291530934b4598a53595c2ed6cd54ba5302aeddc
Meyliasalma <162813299+Meyliasalma@users.noreply.github.com>
2025-10-28 20:44:27 +0700

    Add files via upload

---
+++ tugaspertemuan2/praktikum2.py
@@ -0,0 +1,5 @@
+x = int (input ("memasukkan nilai x: "))
+y = int (input ("memasukkan nilai y: "))
+warna = input ("memasukkan warna titik: ")
+
+print(f"titik berada  di ({x}, {y}) dan berwarna {warna}")
---
+++ tugaspertemuan2/praktikum5.py
@@ -0,0 +1,13 @@
+titik_list =[(0,0), (50,50), (100,0)]
+
+print("Daftar titik:")
+for titik in titik_list:
+    print(titik)
+    
+pusat =(0,0)
+
+print("\nTitik pusat:", pusat)
+
+objek = {"x" : 10, "y": 20, "warna": "biru"}
+
+print (f"\nTitik ({objek['x']},{objek['y']}) berwarna {objek['warna']}.")
